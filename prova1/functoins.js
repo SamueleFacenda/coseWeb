16ab6589b0c8b80f25409ac0da91618f868b77e6
defra <samuele.facenda@gmail.com>
2021-11-27 11:54:56 +0100

    prova

---
+++ prova1/functoins.js
@@ -0,0 +1,9 @@
+let switc=true;
+function cosa1(){
+    if(switc){
+        document.getElementById("nome").innerHTML="on";
+    }else{
+        document.getElementById("nome").innerHTML="on";
+    }
+    switc=!switc;
+}
