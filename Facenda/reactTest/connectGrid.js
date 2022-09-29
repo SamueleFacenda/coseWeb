e26259a12d21c09f96011cf3f671d88cee5da18c
Samuele Facenda <92163673+SamueleFacenda@users.noreply.github.com>
2022-09-29 12:54:48 +0200

    Riorganizzazione per poter consegnare il compito, aggiunti i progetti dei miei compagni

--- Facenda/reactTest/connectGrid.js
+++ Facenda/reactTest/connectGrid.js
@@ -51,6 +51,65 @@ class Board extends React.Component{
     }
 }
 
+class Field extends React.Component{
+    render(){
+        return (
+            <label>
+                {this.props.label}
+                <input type={this.props.type} value={this.props.value} onChange={this.props.onChange}/>
+            </label>
+        )
+    }
+}
+
+class Review extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'Please write a review of this game',
+            email: 'Please enter your email'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('An essay was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    handleChangeEmail(event) {
+        this.setState({email: event.target.value});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Essay:
+                    <textarea value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <label>
+                    email:
+                    <textarea value={this.state.value} onChange={this.handleChangeEmail} />
+                </label>
+                <label>
+
+                    <input type=number value={this.state.value} onChange={this.handleChangeEmail} />
+                </label>
+
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
 var root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Board/>);
 
