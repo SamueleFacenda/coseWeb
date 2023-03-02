66f090dc5d20dd72c6d9835ae60dc22ae51c44b9
SAMUELE.FACENDA <samuele.facenda@buonarroti.tn.it>
2023-03-02 12:52:17 +0100

    center text

--- progettoWeb/website/pages/register.php
+++ progettoWeb/website/pages/register.php
@@ -108,7 +108,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                                             <button type="submit" class="btn btn-primary btn-lg">Register</button>
                                         </div>
-                                        <p>Already a member? <a href="/pages/login.php">Login</a></p>
+                                        <div class="text-center">
+                                            <p>Already a member? <a href="/pages/login.php">Login</a></p>
+                                        </div>
                                     </form>
                                 </div>
                                 <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
