ab5c468cf0ab5f69e35038f668917e0ee343e3b5
Samuele Facenda <samuele.facenda@gmail.com>
2023-03-05 10:38:27 +0100

    add login page

--- progettoWeb/website/pages/login.php
+++ progettoWeb/website/pages/login.php
@@ -1,6 +1,5 @@
 <?php
 require_once '../utils/jwt.php';
-
 require_once '../utils/connection.php';
 
 // get secret from env
@@ -21,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // get user from db
         connect();
 
-        $user = getUser($email);
+        $user = get_user($email);
         if ($user == null) {
             // username not found
             $wrong_credentials = true;
@@ -50,24 +49,52 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <?php
     require_once '../static/head.php';
     echo '<body>';
-    include_once '../utils/jwt.php';
     include_once '../utils/username.php';
     require_once '../static/navbar.php';
 ?>
-    <form action="/pages/login.php" method="post">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" required>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
-        <input type="submit" value="Login">
-    </form>
 
-    <?php if ($wrong_credentials): ?>
-        <h2>Wrong credentials</h2>
-    <?php endif; ?>
-
-    <h2> You're not registered yet? <a href="register.php">Sign up here</a> </h2>
-
+<section class="vh-90" style="background-color: #eee;">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-md-10 col-lg-8 col-xl-6">
+                <div class="card text-black" style="border-radius: 25px;">
+                    <div class="card-body p-md-5">
+                        <div class="row justify-content-center">
+                            <div class="col-md-11 col-lg-10 col-xl-8">
+                                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Log in</p>
+                                <form class="mx-1 mx-md-4" action="/pages/login.php" method="post">
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input name="email" type="email" id="form3Example3c" class="form-control" required
+                                            value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']) ?>" />
+                                            <label class="form-label" for="form3Example3c">Your Email</label>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input name="password" type="password" id="form3Example4c"
+                                                   class="form-control <?php if($wrong_credentials) echo 'is-invalid' ?> " required/>
+                                            <label class="form-label" for="form3Example4c">Password</label>
+                                            <div class="invalid-feedback">Wrong credentials!</div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                        <button type="submit" class="btn btn-primary btn-lg">Log in</button>
+                                    </div>
+                                    <div class="text-center">
+                                        <p>Not registered yet? <a href="/pages/register.php">Sign up</a></p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
     require_once '../static/footer.php';
 ?>
